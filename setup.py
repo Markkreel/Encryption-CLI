b114130d03bebb3d5064179c0b8cee3d397a05ce
Markkreel <chrystmark2020@mail.ugm.ac.id>
2025-03-04 17:32:31 +0700

    Fix: Configured setup.py directories

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="FileLock",
     version="0.1",
-    packages=find_packages(include=["*"]),
+    packages=find_packages(where=".", include=["src*"]),
+    package_dir={"": "."},
     install_requires=[
         "pycryptodome",  # For encryption/decryption functionality
         "pytest",  # For testing
