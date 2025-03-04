0cc1565240ad19291cbf061ca55c67242d8201b3
Markkreel <chrystmark2020@mail.ugm.ac.id>
2025-03-04 18:04:24 +0700

    Fix: Added __init__.py for directory instances

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# FileLock package initialization
-from .compression import compress_file, decompress_file
-
-__all__ = ["compress_file", "decompress_file"]
---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+# Make src directory a proper Python package
+from .compression import *
+from .filelock import *
+from .main import *
